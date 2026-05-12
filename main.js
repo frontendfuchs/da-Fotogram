7fbd96ca09a65754eb757317cde333c86ebaf79e
frontendfuchs <adriana.letitia@yahoo.com>
2026-05-12 03:08:04 +0200

    header footer and rendering  image gallery

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
+const containerRef = document.getElementById("image-gallery");
 
+const imgGallery = ["../assets/images/image1.jpg",
+"../assets/images/image10.png",
+"../assets/images/image11.png",
+"../assets/images/image12.png",
+"../assets/images/image2.jpg",
+"../assets/images/image3.png",
+"../assets/images/image4.jpg",
+"../assets/images/image5.jpg",
+"../assets/images/image6.jpg",
+"../assets/images/image7.jpg",
+"../assets/images/image8.jpg",
+"../assets/images/image9.jpg"
+];
+
+function renderGallery(){
+    for(let i = 0; i < 12; i++){
+        containerRef.innerHTML += /*html*/`
+        <img src="${imgGallery[i]}" onclick="open()">`;
+    } 
+}
+
+renderGallery();
